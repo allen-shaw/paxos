cbe2a931c2468f056b089585665aa5eb7791bee1
allen <allenshaw19@gmail.com>
2022-06-04 01:07:14 +0800

    add instance

--- paxos/propose_batch.go
+++ paxos/propose_batch.go
@@ -1,4 +1,32 @@
 package paxos
 
+import "sync"
+
+type PendingProposal struct {
+	Value *string
+	SMCtx *SMCtx
+
+	// return parameter
+	InstanceID *uint64
+	BatchIndex *uint32
+
+	Notifier       *Notifier
+	AbsEnqueueTime uint64
+}
+
 type ProposeBatch struct {
+	myGroupIdx   int
+	node         Node
+	notifierPool *NotifierPool
+
+	mutex sync.Mutex
+	cond  *sync.Cond
+	queue queue[PendingProposal]
+
+	isEnd     bool
+	isStarted bool
+
+	batchCount       int
+	batchDelayTimeMs int
+	batchMaxSize     int
 }
